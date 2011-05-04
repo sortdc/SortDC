2a76d3320b2c9414df1cf331c0cb4b07e74f32b5
Godefroy <git@skreo.net>
2011-05-04 11:50:38 +0200

    Add: Commentaires + Chargement des fichiers stopwords dans la classe Config

--- src/org/sortdc/sortdc/Config.java
+++ src/org/sortdc/sortdc/Config.java
@@ -1,17 +1,32 @@
 package org.sortdc.sortdc;
 
 import com.esotericsoftware.yamlbeans.YamlReader;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Config {
 
     private static Config instance;
     private Map parameters;
+    public static final String CLASSIFIERS_LIST = "classifiers";
+    public static final String CLASSIFIER_STOPWORDS = "stopwords";
+    public static final String CLASSIFIER_STOPWORDS_FILEPATH = "stopwords_file";
 
     private Config() {
     }
 
+    /**
+     * Creates a unique instance of Config (Singleton)
+     *
+     * @return Instance of Config
+     */
     public static synchronized Config getInstance() {
         if (instance == null) {
             instance = new Config();
@@ -19,12 +34,69 @@ public class Config {
         return instance;
     }
 
+    /**
+     * Loads configuration parameters from a config file
+     *
+     * @param filePath Path to the config file
+     * @throws Exception
+     */
     public void loadFile(String filePath) throws Exception {
         YamlReader reader = new YamlReader(new FileReader(filePath));
         this.parameters = (Map) reader.read();
+
+        this.loadStopWords();
     }
 
+    /**
+     * Loads stopwords for each classifier from stopwords files
+     *
+     * @throws Exception
+     */
+    private void loadStopWords() throws Exception {
+        List<Map> classifiers = (List) this.get(CLASSIFIERS_LIST);
+        if (classifiers != null) {
+            for (Map classifier : classifiers) {
+                String stopwordsFilepath = (String) classifier.get(CLASSIFIER_STOPWORDS_FILEPATH);
+                if (stopwordsFilepath == null) {
+                    continue;
+                }
+                List<String> stopWords = new ArrayList();
+                try {
+                    InputStream is = new FileInputStream(stopwordsFilepath);
+                    InputStreamReader isr = new InputStreamReader(is);
+                    BufferedReader br = new BufferedReader(isr);
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        stopWords.add(line.trim());
+                    }
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+                if (stopWords.size() != 0 || classifier.get(CLASSIFIER_STOPWORDS) == null) {
+                    classifier.put(CLASSIFIER_STOPWORDS, stopWords);
+                }
+            }
+        }
+    }
+
+    /**
+     * Get a parameter
+     *
+     * @param parameter Name of the parameter
+     * @return Value of the parameter
+     */
     public Object get(String parameter) {
         return this.parameters.get(parameter);
+    }
+
+    /**
+     * Set a parameter's value
+     * 
+     * @param name Name of the parameter
+     * @param value Value of the parameter
+     */
+    public void set(String name, Object value) {
+        this.parameters.put(name, value);
     }
 }
--- src/org/sortdc/sortdc/Main.java
+++ src/org/sortdc/sortdc/Main.java
@@ -1,13 +1,6 @@
 package org.sortdc.sortdc;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
 
@@ -15,53 +8,14 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-        List<String> stopWords = new ArrayList();
+        Config config = Config.getInstance();
         try {
-            InputStream is = new FileInputStream("config/stopwords.txt");
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            String line;
-            while ((line = br.readLine()) != null) {
-                stopWords.add(line.trim());
-            }
-            br.close();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-        Tokenization tokenization = new Tokenization();
-        //tokenization.setExtractWords(false);
-        tokenization.setApplyStemming(true);
-        tokenization.setWordsMinLength(3);
-        //tokenization.setExtractBigrams(true);
-        //tokenization.setExtractTrigrams(true);
-        tokenization.setStopWords(stopWords);
-
-        List test = tokenization.extract("Bonjour tout le monde les applications de classification sont jolies éè à ö légèrement. Porte-monnaies! Hello World =)\nbonjour!", "french");
-
-        /*for (int i = 0; i < test.size(); i++) {
-        System.out.println(test.get(i));
-        }*/
-
-        try {
-            Config config = Config.getInstance();
             config.loadFile("config/config.yaml");
-
-            Map db = (Map) config.get("database");
-
-            System.out.println(db.get("host"));
-            System.out.println(db.get("port"));
-            System.out.println(db.get("dbname"));
-            System.out.println(db.get("username"));
-            System.out.println(db.get("password"));
-
-            List instances = (List) config.get("instances");
-
-            System.out.println(instances);
-
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
+        List classifiers = (List) config.get(Config.CLASSIFIERS_LIST);
+        System.out.println(classifiers);
     }
 }
