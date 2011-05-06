2c629633a3d437375dc8b5ab6654f4754ef90062
Ronan <ronanlet@gmail.com>
2011-05-06 19:21:29 +0200

    ADD : méthode de récupération des catégories pour MySQL

--- src/org/sortdc/sortdc/database/DatabaseMysql.java
+++ src/org/sortdc/sortdc/database/DatabaseMysql.java
@@ -2,6 +2,11 @@ package org.sortdc.sortdc.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import org.sortdc.sortdc.dao.Category;
 
 public class DatabaseMysql extends Database {
 
@@ -23,7 +28,31 @@ public class DatabaseMysql extends Database {
         return instance;
     }
 
+    /**
+     * Establishes connection with database
+     *
+     * @throws Exception
+     */
     public void connect() throws Exception {
         this.db = DriverManager.getConnection("jdbc:mysql://" + this.host + "/" + this.db_name, this.username, this.password);
+    }
+
+    /**
+     * Finds all registered categories
+     *
+     * @return Categories list
+     * @throws Exception
+     */
+    public List<Category> findAllCategories() throws Exception {
+        List<Category> categories = new ArrayList();
+        Statement statement = this.db.createStatement();
+        ResultSet data = statement.executeQuery("SELECT * FROM categories");
+        while (data.next()) {
+            Category category = new Category();
+            category.setId(data.getString("id"));
+            category.setName(data.getString("name"));
+            categories.add(category);
+        }
+        return categories;
     }
 }
