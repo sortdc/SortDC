b2759e590731b39402211f0be44e5d94b06b0943
Ronan <ronanlet@gmail.com>
2011-05-06 19:43:30 +0200

    Add: Méthode d'insertion / sauvegarde de catégories pour MySQL

--- src/org/sortdc/sortdc/database/DatabaseMysql.java
+++ src/org/sortdc/sortdc/database/DatabaseMysql.java
@@ -2,6 +2,7 @@ package org.sortdc.sortdc.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -11,7 +12,7 @@ import org.sortdc.sortdc.dao.Category;
 public class DatabaseMysql extends Database {
 
     private static Database instance;
-    private Connection db;
+    private Connection connection;
 
     private DatabaseMysql() {
     }
@@ -34,7 +35,7 @@ public class DatabaseMysql extends Database {
      * @throws Exception
      */
     public void connect() throws Exception {
-        this.db = DriverManager.getConnection("jdbc:mysql://" + this.host + "/" + this.db_name, this.username, this.password);
+        this.connection = DriverManager.getConnection("jdbc:mysql://" + this.host + "/" + this.db_name, this.username, this.password);
     }
 
     /**
@@ -45,7 +46,7 @@ public class DatabaseMysql extends Database {
      */
     public List<Category> findAllCategories() throws Exception {
         List<Category> categories = new ArrayList();
-        Statement statement = this.db.createStatement();
+        Statement statement = this.connection.createStatement();
         ResultSet data = statement.executeQuery("SELECT * FROM categories");
         while (data.next()) {
             Category category = new Category();
@@ -54,5 +55,29 @@ public class DatabaseMysql extends Database {
             categories.add(category);
         }
         return categories;
+    }
+
+    /**
+     * Saves or updates a category
+     *
+     * @param category
+     * @throws Exception
+     */
+    public void saveCategory(Category category) throws Exception {
+        if (category.getId() != null) {
+            PreparedStatement statement = this.connection.prepareStatement("INSERT INTO categories (id, name) VALUES(?,?)");
+            statement.setString(1, category.getId());
+            statement.setString(2, category.getName());
+            statement.execute();
+        } else {
+            PreparedStatement statement = this.connection.prepareStatement("UPDATE categories SET name = ? WHERE id = ?");
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getId());
+            statement.execute();
+            if (statement.getUpdateCount() == 0) {
+                category.setId(null);
+                this.saveCategory(category);
+            }
+        }
     }
 }
